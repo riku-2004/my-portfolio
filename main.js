6ff048f4f34d7bacb8bc432b5f119cc1dfe41f04
riku-2004 <lubii2004@gmail.com>
2024-11-19 02:55:27 +0900

    タブナビゲーションを追加し、自己紹介ボタンの機能を修正しました。スタイルを調整し、ボタンのデザインを改善しました。

--- main.js
+++ main.js
@@ -1,10 +1,16 @@
-const aboutMyself = document.getElementById("myselfButton");
 const myName = document.getElementById("name");
 const myBirthday = document.getElementById("birthday");
 const myHometown = document.getElementById("hometown");
+const about = document.getElementById("about");
 
-aboutMyself.onclick = function () {
+about.onclick = function () {
   myName.textContent = "名前：並木陸";
   myBirthday.textContent = "生年月日:2004年1月15日";
   myHometown.textContent = "出身地:長野県軽井沢町";
 };
+
+const tabs = document.querySelectorAll("tabControl");
+const pages = document.querySelectorAll("tabBody");
+
+function changePage(index) {
+  
